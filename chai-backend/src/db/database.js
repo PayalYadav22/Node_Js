c700a845dadfdb5a923e29c773a09ec657392287
Payal Yadav <pty22112000@gmail.com>
2025-01-04 03:20:12 -0800

    database connection

--- chai-backend/src/db/database.js
+++ chai-backend/src/db/database.js
@@ -4,9 +4,9 @@ import { DB_NAME } from '../constants.js';
 const connectDB = async () => {
     try{
         const connect = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-        console.log(`MongoDB Connected : ${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`MongoDB Connection Success : ${process.env.MONGO_URI}/${DB_NAME}`);
     }catch(error){
-        console.error(`MongoDB Connection Failed ${error.message}`);
+        console.error(`MongoDB Connection Failed : ${error.message}`);
         process.exit(1)
     }
 }
