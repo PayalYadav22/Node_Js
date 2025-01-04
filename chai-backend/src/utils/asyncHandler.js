1616a9906daea7984b0cad7b7286359bc6b7ca92
Payal Yadav <pty22112000@gmail.com>
2025-01-04 05:20:44 -0800

    ApiError & ApiResponse Handler

--- chai-backend/src/utils/asyncHandler.js
+++ chai-backend/src/utils/asyncHandler.js
@@ -1,9 +1,8 @@
-// const asyncHandler = () => {
-
-// }
-
-const asyncHandler =  (fn) => () => {
-    
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error)=> next(error))
+    }
 }
 
 export default asyncHandler;
